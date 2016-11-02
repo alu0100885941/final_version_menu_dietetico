14089c2320aa31a3d7487849d49876c6a45310cc
Sergio Morente <alu0100885941@ull.edu.es>
2016-11-02 12:54:38 +0000

    Prueba 2: Tiene que haber un porcentaje. FALLO

--- spec/dieta_spec.rb
+++ spec/dieta_spec.rb
@@ -4,13 +4,19 @@ require "./lib/dieta.rb"
 context 'Creando la clase a probar' do
 	before:each do
 	
-		@desayuno = Dieta::Menu.new("Desayuno")
+		@desayuno = Dieta::Menu.new("Desayuno", [15, 20])
+		
 	end
 
 describe 'Prueba 1: Titulo.' do
 		it 'Tiene que haber un titulo' do
 			expect(@desayuno.titulo).to eq("Desayuno")
 		end
+end
+
+describe 'Prueba 2: Porcentajes.' do
+		it 'Debe existir un rango de porcentajes en cada ingesta de la dieta' do
+			expect(@desayuno.porcentaje).to eq([15, 20]);
 end
 end	
 
