cee00cc49637dbb9f01a3b0fa2b90f1426841180
Sergio Morente <alu0100885941@ull.edu.es>
2016-11-02 12:05:33 +0000

    Prueba 1: Tiene que haber un titulo. FALLO

--- spec/dieta_spec.rb
+++ spec/dieta_spec.rb
@@ -1,6 +1,15 @@
 require "spec_helper"
 require "./lib/dieta"
 
-describe Dieta do
-	
+context 'Creando la clase a probar' do
+	before:each do
+
+		@desayuno = Dieta::Menu.new('Desayuno')
+	end
 end
+describe 'Prueba 1: Titulo' do
+		it 'Tiene que haber un titulo' do
+			expect(@desayuno.titulo).to eq('Desayuno')
+		end
+end	
+
