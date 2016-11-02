02927dd916d9145f33af65a7ccbc5bb23b23e3d4
Sergio Morente <alu0100885941@ull.edu.es>
2016-11-02 11:44:22 +0000

    Primera prueba, cargando fichero, prueba exitosa.

--- spec/dieta_spec.rb
+++ spec/dieta_spec.rb
@@ -1,11 +1,6 @@
 require "spec_helper"
+require "./lib/dieta"
 
 describe Dieta do
-  it "has a version number" do
-    expect(Dieta::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	
 end
