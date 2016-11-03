38ed0080b41a9a686f64f62f300e6c0ba1783199
Jaime Madico <alu0100895179@ull.edu.es>
2016-11-03 17:25:07 +0000

    Pruebas con porcentajes de proteinas grasas e hidratos exitosas.

--- lib/dieta.rb
+++ lib/dieta.rb
@@ -3,9 +3,9 @@ require "dieta/version"
 module Dieta
   class Menu
   	
-	attr_reader :titulo, :porcentaje, :platos, :vct
-	def initialize(titulo, porcentaje, platos, vct)
-		@titulo, @porcentaje, @platos, @vct= titulo, porcentaje, platos,vct
+	attr_reader :titulo, :porcentaje, :platos, :vct, :porcentaje_pgh
+	def initialize(titulo, porcentaje, platos, vct, porcentaje_pgh)
+		@titulo, @porcentaje, @platos, @vct, @porcentaje_pgh= titulo, porcentaje, platos, vct, porcentaje_pgh
 	end
 
   end
