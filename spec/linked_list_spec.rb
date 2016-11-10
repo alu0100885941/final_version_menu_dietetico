2c3f94f382ddbcebff4b0656fa3e3d66d9fef4aa
Sergio Morente <alu0100885941@ull.edu.es>
2016-11-10 20:57:36 +0000

    Prueba de formateo añadida.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -26,6 +26,16 @@ describe "Probando extract" do
 		expect(@lista.extract).to eq(1)
 	end
 end
-		
 
+describe "Probando metodo  to_s" do
+	it "El formateo de la lista debe ser correcto." do
+		@lista.insert(4)
+		@lista.insert(5)
+		@lista.insert(6)
+		@lista.insert(1)
+		@lista.insert(2)
+		@lista.insert(3)
+		expect(@lista.to_s).to eq("Valor numero 1) Equivale a 3\nValor numero 2) Equivale a 2\nValor numero 3) Equivale a 1\nValor numero 4) Equivale a 6\nValor numero 5) Equivale a 5\nValor numero 6) Equivale a 4\n")
+	end
+	end		
 end
